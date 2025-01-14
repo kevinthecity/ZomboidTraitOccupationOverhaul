0e3a9f0957dd490fd260cab87a6ed585bfdb534b
Kevin Grant <kevingrant5@gmail.com>
2025-01-13 21:12:10 -0500

    Try simple thing

---
+++ 42/media/lua/shared/MyModMain.lua
@@ -0,0 +1,7 @@
+if ProfessionFramework then
+    ProfessionFramework.addTrait('Brave2', {
+        cost = -4, -- Adjusted cost for the Brave trait
+    })
+else
+    print("Profession Framework is not loaded.")
+end
