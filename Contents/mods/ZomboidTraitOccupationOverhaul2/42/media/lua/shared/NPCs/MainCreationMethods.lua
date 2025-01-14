647baf56467e91de19d45256e2d7615644161721
Kevin Grant <kevingrant5@gmail.com>
2025-01-13 19:47:51 -0500

    A bit of cleanip

---
+++ Contents/mods/ZomboidTraitOccupationOverhaul2/42/media/lua/shared/NPCs/MainCreationMethods.lua
@@ -0,0 +1,12 @@
+ProfessionFramework.addTrait('Hunter', {
+    name = "UI_prof_parkranger",
+    description = "UI_trait_HunterDesc",
+    cost = 8,
+    xp = {
+        [Perks.Aiming] = 2,
+        [Perks.Trapping] = 3,
+        [Perks.Sneak] = 4,
+        [Perks.SmallBlade] = 5,
+    },
+    recipes = {"Make Stick Trap", "Make Snare Trap", "Make Wooden Cage Trap", "Make Trap Box", "Make Cage Trap"},        
+})
