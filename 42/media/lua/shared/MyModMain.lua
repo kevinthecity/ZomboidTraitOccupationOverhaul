d1fee626fa352835aade5adc522cce370158e558
Kevin Grant <kevingrant5@gmail.com>
2025-01-13 21:16:08 -0500

    Check for perm framework

--- 42/media/lua/shared/MyModMain.lua
+++ 42/media/lua/shared/MyModMain.lua
@@ -1,12 +1,16 @@
-ProfessionFramework.addTrait('Hunter', {
-    name = "UI_prof_parkranger",
-    description = "UI_trait_HunterDesc",
-    cost = 8,
-    xp = {
-        [Perks.Aiming] = 2,
-        [Perks.Trapping] = 3,
-        [Perks.Sneak] = 4,
-        [Perks.SmallBlade] = 5,
-    },
-    recipes = {"Make Stick Trap", "Make Snare Trap", "Make Wooden Cage Trap", "Make Trap Box", "Make Cage Trap"},        
-})
+if ProfessionFramework then
+    ProfessionFramework.addTrait('Hunter', {
+        name = "UI_prof_parkranger",
+        description = "UI_trait_HunterDesc",
+        cost = 8,
+        xp = {
+            [Perks.Aiming] = 2,
+            [Perks.Trapping] = 3,
+            [Perks.Sneak] = 4,
+            [Perks.SmallBlade] = 5,
+        },
+        recipes = {"Make Stick Trap", "Make Snare Trap", "Make Wooden Cage Trap", "Make Trap Box", "Make Cage Trap"},        
+    })
+else
+    print("ProfessionFramework not found")
+end
